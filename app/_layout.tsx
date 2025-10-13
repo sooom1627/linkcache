101e050d7565055b314351f9635bea3ec7a29321
soma <swfh2367@gmail.com>
2025-10-13 15:53:42 +0900

    feat: implement authentication flow in RootLayout with protected routes for signed-in and guest users

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,17 @@ import { Stack } from "expo-router";
 import "../assets/styles/global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const isAuthenticated = false;
+
+  return (
+    <Stack>
+      <Stack.Protected guard={isAuthenticated}>
+        <Stack.Screen name="(tabs)" />
+      </Stack.Protected>
+      <Stack.Protected guard={!isAuthenticated}>
+        <Stack.Screen name="sign-in" />
+        <Stack.Screen name="create-account" />
+      </Stack.Protected>
+    </Stack>
+  );
 }
---
+++ app/create-account.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function CreateAccount() {
+  return (
+    <View>
+      <Text>CreateAccount</Text>
+    </View>
+  );
+}
