e8c537cb222d68b6e266c3801511b52b5c3a7639
soma <swfh2367@gmail.com>
2025-10-28 07:20:03 +0900

    test: setup Jest with Level 1 timezone tests

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: "jest-expo",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/__tests__/**",
+  ],
+};
---
+++ jest.setup.js
@@ -0,0 +1,2 @@
+// Jest setup file
+// @testing-library/react-native v12.4+ includes matchers by default
