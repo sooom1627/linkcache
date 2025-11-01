537c552a2bd033b65cf2ea8ea3f2608ed795b06a
soma <swfh2367@gmail.com>
2025-11-01 12:23:24 +0900

    refactor: improve layout and readability in LanguageSettings component
    - Adjusted text alignment and added a wrapper for the language readiness label to enhance visual clarity.
    - Ensured consistent styling for text elements within the LanguageSettings component.

--- src/features/users/components/setting/LanguageSettings.tsx
+++ src/features/users/components/setting/LanguageSettings.tsx
@@ -61,11 +61,15 @@ export default function LanguageSettings() {
             className={`min-w-[40%] flex-1 flex-col flex-wrap items-center justify-start gap-2 rounded-lg border bg-slate-100
             p-4 ${selectedLanguage === language.code ? "border-blue-500" : "border-slate-200"} ${!language.isReady ? "opacity-50" : ""}`}
           >
-            <Text className="font-bold text-slate-700">
+            <Text className="w-full text-left font-bold text-slate-700">
               {language.flag} {language.name}{" "}
-              {!language.isReady && "(Not Ready)"}
+              <Text className="font-normal text-slate-500">
+                {!language.isReady && "(Not Ready)"}
+              </Text>
             </Text>
-            <Text className="text-slate-500">{language.text}</Text>
+            <Text className="w-full text-left text-slate-500">
+              {language.text}
+            </Text>
           </TouchableOpacity>
         ))}
       </View>
